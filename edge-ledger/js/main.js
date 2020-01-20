0d711da0b2d5c0c0e5d2323171280027632e19c9
Chris Gartsen <chris@gartsen.nl>
2020-01-20 17:39:42 +0100

    Add opactity on navbar

--- edge-ledger/js/main.js
+++ edge-ledger/js/main.js
@@ -7,6 +7,14 @@ function initMap() {
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
 $("#navbar a, .btn").on("click", function(event) {
   if (this.hash !== "") {
     event.preventDefault();
